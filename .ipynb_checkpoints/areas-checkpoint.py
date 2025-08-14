f303d2904f2453c684d708acdaf4a23e4a553a3a
Hridik Dev <hridikori@gmail.com>
2025-08-14 13:55:36 +0530

    First commit

---
+++ .ipynb_checkpoints/areas-checkpoint.py
@@ -0,0 +1,8 @@
+def acircle(r):
+	return 3.14*r*r
+
+def arectangle(l,b):
+	return l*b
+
+def atriangle(b,h):
+	return 0.5*b*h
---
+++ .ipynb_checkpoints/hw-checkpoint.py
@@ -0,0 +1,10 @@
+
+a=2
+print(id(a))
+
+blah = 1
+
+print(blah)
+
+KmPerHour = 23
+print(KmPerHour)
